163c0913601ccd4f12ad1a641d51b054f9a5260f
Kashionz <kashionz.nicon.s@gmail.com>
2020-11-30 00:03:43 +0800

    Test version

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Evision_profile_detec
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbimg = new System.Windows.Forms.PictureBox();
+            this.btnDetect = new System.Windows.Forms.Button();
+            this.labelSpeed = new System.Windows.Forms.Label();
+            this.openFile = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.Value_In = new System.Windows.Forms.TextBox();
+            this.Value_Out = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbimg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbimg
+            // 
+            this.pbimg.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbimg.Location = new System.Drawing.Point(573, 80);
+            this.pbimg.Margin = new System.Windows.Forms.Padding(4);
+            this.pbimg.Name = "pbimg";
+            this.pbimg.Size = new System.Drawing.Size(562, 425);
+            this.pbimg.TabIndex = 0;
+            this.pbimg.TabStop = false;
+            // 
+            // btnDetect
+            // 
+            this.btnDetect.Font = new System.Drawing.Font("新細明體", 9F);
+            this.btnDetect.Location = new System.Drawing.Point(185, 15);
+            this.btnDetect.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDetect.Name = "btnDetect";
+            this.btnDetect.Size = new System.Drawing.Size(161, 58);
+            this.btnDetect.TabIndex = 1;
+            this.btnDetect.Text = "Detect";
+            this.btnDetect.UseVisualStyleBackColor = true;
+            this.btnDetect.Click += new System.EventHandler(this.btnDetect_Click);
+            // 
+            // labelSpeed
+            // 
+            this.labelSpeed.AutoSize = true;
+            this.labelSpeed.Font = new System.Drawing.Font("新細明體", 20F);
+            this.labelSpeed.Location = new System.Drawing.Point(567, 23);
+            this.labelSpeed.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelSpeed.Name = "labelSpeed";
+            this.labelSpeed.Size = new System.Drawing.Size(111, 34);
+            this.labelSpeed.TabIndex = 2;
+            this.labelSpeed.Text = "Speed: ";
+            // 
+            // openFile
+            // 
+            this.openFile.Font = new System.Drawing.Font("新細明體", 9F);
+            this.openFile.Location = new System.Drawing.Point(16, 15);
+            this.openFile.Margin = new System.Windows.Forms.Padding(4);
+            this.openFile.Name = "openFile";
+            this.openFile.Size = new System.Drawing.Size(161, 58);
+            this.openFile.TabIndex = 3;
+            this.openFile.Text = "Open Folder";
+            this.openFile.UseVisualStyleBackColor = true;
+            this.openFile.Click += new System.EventHandler(this.openFile_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(16, 80);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(548, 424);
+            this.listBox1.TabIndex = 4;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Value_In
+            // 
+            this.Value_In.Location = new System.Drawing.Point(969, 12);
+            this.Value_In.Name = "Value_In";
+            this.Value_In.Size = new System.Drawing.Size(166, 25);
+            this.Value_In.TabIndex = 5;
+            this.Value_In.Text = "930000";
+            // 
+            // Value_Out
+            // 
+            this.Value_Out.Location = new System.Drawing.Point(969, 48);
+            this.Value_Out.Name = "Value_Out";
+            this.Value_Out.Size = new System.Drawing.Size(166, 25);
+            this.Value_Out.TabIndex = 6;
+            this.Value_Out.Text = "576793";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(876, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "臨界值(In):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(876, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "臨界值(Out):";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1163, 530);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Value_Out);
+            this.Controls.Add(this.Value_In);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.openFile);
+            this.Controls.Add(this.labelSpeed);
+            this.Controls.Add(this.btnDetect);
+            this.Controls.Add(this.pbimg);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Vehicle Speed Measurement";
+            ((System.ComponentModel.ISupportInitialize)(this.pbimg)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbimg;
+        private System.Windows.Forms.Button btnDetect;
+        private System.Windows.Forms.Label labelSpeed;
+        private System.Windows.Forms.Button openFile;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox Value_In;
+        private System.Windows.Forms.TextBox Value_Out;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Euresys.Open_eVision_2_12;
+
+namespace Evision_profile_detec
+{
+    public partial class Form1 : Form
+    {
+        EImageC24 EC24Image1 = new EImageC24(); //eVision的彩色圖像物件
+        EImageBW8 EBW8Image1 = new EImageBW8(); //eVision的灰階圖像物件
+
+        EBW8Vector In = new EBW8Vector(); // EBW8Vector instance
+        EBW8Vector Out = new EBW8Vector(); // EBW8Vector instance
+        EColorLookup EColorLookup1 = new EColorLookup();
+
+        string[] files;
+        float ScalingRatio = 0; //Picturebox與原始影像大小的縮放比例
+
+        double last_In = 0, last_Out = 0;
+        double valuein = 7.8;
+        double valueout = 7.8;
+        int previous = 0, current = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void openFile_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = Application.StartupPath;
+            listBox1.Items.Clear();
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                files = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.jpg").OrderBy(x => x.Length).ThenBy(x => x).ToArray();
+                foreach (string f in files)
+                {
+                    listBox1.Items.Add(f);
+                }
+            }
+        }
+
+        private void btnDetect_Click(object sender, EventArgs e)
+        {
+            double t1 = 0, t2 = 0, timeDiff = 0, fov = 5.0, speed = 0;
+            bool hascar = false;
+            valuein = double.Parse(Value_In.Text);
+            valueout = double.Parse(Value_Out.Text);
+            last_In = 0;
+            last_Out = 0;
+
+            EC24Image1.Load(files[0]);
+
+            /*============================計算scaling ratio============================*/
+            float PictureBoxSizeRatio = (float)pbimg.Width / pbimg.Height;
+            float ImageSizeRatio = (float)EC24Image1.Width / EC24Image1.Height;
+            if (ImageSizeRatio > PictureBoxSizeRatio)
+                ScalingRatio = (float)pbimg.Width / EC24Image1.Width;
+            else
+                ScalingRatio = (float)pbimg.Height / EC24Image1.Height;
+            /*=========================================================================*/
+
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                listBox1.SelectedIndex = i;
+                listBox1.Refresh();
+                EC24Image1.Load(files[i]);
+                EC24Image1.Draw(pbimg.CreateGraphics(),ScalingRatio);
+
+                EBW8Image1.SetSize(EC24Image1);
+                EasyImage.Oper(EArithmeticLogicOperation.Copy, new EBW8(0), EBW8Image1);
+                EasyImage.Convert(EC24Image1, EBW8Image1); //轉灰階
+
+                EasyImage.ImageToLineSegment(EBW8Image1, In, 1000, 1079, 1750, 1079); //設定偵測線在最底部，判斷車子是否準備進來
+                EasyImage.ImageToLineSegment(EBW8Image1, Out, 1000, 0, 1750, 0); //設定偵測線在最頂部，判斷車子是否準備出去
+
+                if (CarIn(In) && !hascar) //假如沒有車子，且CarIn有偵測到車子情況下，視同進入
+                {
+                    previous = i; //記錄車子進來的圖片張數
+                    //t1 = DateTime.Now.TimeOfDay.Seconds + DateTime.Now.TimeOfDay.Milliseconds / 1000.0; //記錄進來的時間
+                    speed = 0;
+                    hascar = true;
+                }
+                else if (CarOut(Out) && hascar && speed == 0) //假如有車子，且CarOut有偵測到車子並尚未計算速度的情況下，視同出去
+                {
+                    current = i; //記錄車子出去的圖片張數
+                    if (current - previous <= 10 || current - previous >= 100) //防呆機制，假如10張內或超過100張視同有問題
+                    {
+                        //last_Out = 0;
+                        hascar = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("車子進來的第一張圖片:" + files[previous]);
+                        Console.WriteLine("車子出去的最後一張圖片:" + files[current]);
+                        t1 = Double.Parse(Path.GetFileNameWithoutExtension(files[previous])); //擷取進來的圖檔名當作進來的時間
+                        t2 = Double.Parse(Path.GetFileNameWithoutExtension(files[current])); //擷取出去的圖檔名當作出去的時間
+                        //t2 = DateTime.Now.TimeOfDay.Seconds + DateTime.Now.TimeOfDay.Milliseconds / 1000.0; //記錄出去的時間
+                        Console.WriteLine("T1=" + t1.ToString());
+                        Console.WriteLine("T2=" + t2.ToString());
+                        //秒數做溢位處理
+                        /*
+                        if (t1 > t2)
+                            timeDiff = (t2 + 60) - t1;
+                        else
+                        */
+                        timeDiff = t2 - t1; //出去的時間減去進來的時間
+                        Console.WriteLine("timeDiff = " + timeDiff);
+                        speed = fov / (timeDiff / 4) * 3.6; //計算車子的速度，因為採用4倍慢速錄影所以時間要除以4，速度單位為km/hr
+                        speed = Math.Round(speed, 2, MidpointRounding.AwayFromZero); //將速度四捨五入
+                        labelSpeed.Text = "Speed: " + speed.ToString();
+                        Console.WriteLine("Speed: " + speed);
+                        labelSpeed.Update();
+                        last_In = 0;
+                        hascar = false;
+                    }
+                }
+            }
+        }
+
+        private bool CarIn(EBW8Vector State)
+        {
+            double sum = 0, max, min;
+            for (int i = 0; i < State.NumElements; i++)
+            {
+                sum += Math.Pow(State.GetElement(i).Value, 2);
+            }
+            if (last_In == 0)
+            {
+                last_In = sum;
+                return false;
+            }
+
+            max = last_In + valuein;
+            min = last_In - valuein;
+
+            if (min < sum && sum < max)
+            {
+                last_In = sum;
+                return false;
+            }
+            else
+            {
+                last_In = sum;
+                return true;
+            }
+        }
+
+        private bool CarOut(EBW8Vector State)
+        {
+            double sum = 0, max, min;
+            for (int i = 0; i < State.NumElements; i++)
+            {
+                sum += Math.Pow(State.GetElement(i).Value, 2);
+            }
+            if (last_Out == 0)
+            {
+                last_Out = sum;
+                return false;
+            }
+
+            max = last_Out + valueout;
+            min = last_Out - valueout;
+
+            if (min < sum && sum < max)
+            {
+                last_Out = sum;
+                return false;
+            }
+            else
+            {
+                last_Out = sum;
+                return true;
+            }
+        }
+    }
+}
